991349edac01ebbaa49ccc456ad06a9dfccbd6bd
곽윤호 <kwakdm95@gmail.com>
2024-10-04 15:25:42 +0900

    math(수학)모듈 수입

---
+++ first.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+print(a + b)
+
+print(1) # int
+print(0)
+print(-1)
+print(1.1) # float
+
+import math
+
+print('math.sqrt(4)',math.sqrt(4)) # square root
+print('math.pow(4,2)',math.pow(4,2)) # power
