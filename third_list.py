d5bea9fa844046533b433d115cd69b50254a7727
곽윤호 <kwakdm95@gmail.com>
2024-10-04 15:54:39 +0900

    statistics = 통계 모듈

---
+++ third_list.py
@@ -0,0 +1,10 @@
+student = ['John', 'Doe', 'New York']
+grades = [2, 4, 1, 3]
+
+import statistics
+
+print('min(grades)', min(grades))
+print('max(grades)', max(grades))
+print('sum(grades)', sum(grades))
+
+print('statistics.mean(grades)', statistics.mean(grades))
